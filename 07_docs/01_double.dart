570b52bd30123fecf5b55ee5a9e8f69c2a1c0baf
MiltonVera <a00833471@tec.mx>
2022-05-16 08:18:09 -0500

    docs

--- 07_docs/01_double.dart
+++ 07_docs/01_double.dart
@@ -1,12 +1,26 @@
 
 
-main(){
-
-  double numero = 3.1416;
-
-  print("Firma $numero");//Convierte el double a string con el metodo toString()
-
-  print("Firma $numero.sign");
+main(List<String> args) {
   
+  double numero = 2.1416;
+  double infinito = double.infinity;
+
+  print('Firma: ${ numero.sign } :: $numero');
+
+  print('isFinite: ${ numero.isFinite } :: $numero');
+  print('isFinite: ${ infinito.isFinite } :: $infinito');
+
+  print('abs: ${ numero.abs() } :: $numero');
+  print('ceil: ${ numero.ceil() } :: $numero');
+  
+  // print('ceil: ${ infinito.ceil() } :: $numero');
+
+  print('ceilToDouble: ${ numero.ceilToDouble() } :: $numero');
+  
+  print('round: ${ numero.round() } :: $numero');
+  print('round: ${ numero.roundToDouble() } :: $numero');
+  
+  print('clamp: ${ numero.clamp(1, 3) } :: $numero');
+
 
 }
